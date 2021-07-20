efa76fdc9b051f2cad730c5b45c1e7e4b72393b4
ZeromadHero <rene.huber00@gmx.at>
2021-07-20 13:01:24 +0200

    Initial Release

---
+++ main.py
@@ -0,0 +1,111 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+import sys
+import time
+
+#keywords to look for
+keywords = ['Anime', 'Otaku', 'Weeb', 'Gaming', 'Discord', 'Weeaboo', 'Games', 'Japan', 'Japanese', 'Japanisch', 'Nerd']
+
+#webdriver settings
+PATH = "msedgedriver.exe"
+driver = webdriver.Edge(PATH)
+driver.maximize_window()
+
+#function to clear console
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+#function to set console font color
+def SetConsoleColor(Color):
+    if Color.lower() == 'green':
+        sys.stdout.write("\033[0;32m")
+    elif Color.lower() == 'red':
+        sys.stdout.write("\033[1;31m")
+    elif Color.lower() == 'reset':
+        sys.stdout.write("\033[0;0m")
+
+#main swipe function
+def Swipe():
+
+    bioElem = '//*[@id="q633216204"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div[3]'  
+    popup1 = '//*[@id="q-1095164872"]/div/div/div[2]/button[2]'
+
+    while(True):
+
+        #create key action
+        action = ActionChains(driver)
+
+        #kill popups if there are any
+        try:
+            driver.find_element_by_xpath(popup1).click()
+        except:
+            None
+        
+        #get current time for timestamp
+        time.sleep(1.5)
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+
+        #get biography text
+        hit = False
+        hitWord = ''
+        bioText = driver.find_element_by_xpath(bioElem).text
+
+        #check if one of the keywords exists in the biography
+        for keyword in keywords:
+            if keyword in bioText or keyword.lower() in bioText or keyword.upper() in bioText:
+                hit = True
+                hitWord = keyword
+                break
+        
+        #if keyword exists wait for input
+        if hit:
+            SetConsoleColor('Green')
+            print('[{}] Found profile with keyword "{}".\nLike? (Y/N)\n'.format(current_time, hitWord))
+            SetConsoleColor('Reset')
+            x = input('')
+            if x.lower() == 'y':
+                action.send_keys(Keys.ARROW_RIGHT)
+                action.perform()
+            else:
+                action.send_keys(Keys.ARROW_LEFT)
+                action.perform()
+        else:
+            SetConsoleColor('Red')
+            print('[{}] Criteria not matched\n'.format(current_time))
+            SetConsoleColor('Reset')
+            action.send_keys(Keys.ARROW_LEFT)
+            action.perform()
+    
+#initial start actions
+def init():
+    driver.get("https://tinder.com/app/recs")
+
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="q633216204"]/div/div[2]/div/div/div[1]/button').click()
+    driver.find_element_by_xpath('//*[@id="q633216204"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]').click()
+    cls()
+
+    SetConsoleColor('Green')
+    print('Please sign in and wait for the next page to load.\nContinue? (Y/N)\n')
+    SetConsoleColor('Reset')
+    x = input('')
+    if x.lower() == 'y':
+        cls()
+        Swipe()
+    else:
+        driver.close()
+
+#startup
+cls()
+SetConsoleColor('Green')
+print('Welcome to Tin Bot ©2021, Rene Huber')
+print('Current keywords: {}\nStart? (Y/N)\n'.format(keywords))
+SetConsoleColor('Reset')
+x = input('')
+if x.lower() == 'y':
+    init()
+else:
+    driver.close()
