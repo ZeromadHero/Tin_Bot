b77fa5d9bad6395f5947f5fb04b1ed8a5318082f
ZeromadHero <rene.huber00@gmx.at>
2022-02-16 11:55:00 +0100

    Menu rework and autolike added

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import time
 keywords = ['Anime', 'Otaku', 'Weeb', 'Gaming', 'Discord', 'Weeaboo', 'Games', 'Japan', 'Japanese', 'Japanisch', 'Nerd', 'Asia', 'アニメ']
 #red flags which will not trigger an input prompt
 redFlags = ['Pronouns', 'She/they', 'Asexual', 'Vaxx', 'Vaccinated', 'Impf']
+#automatically like profiles that meet the criteria
+autolike = False
 
 #webdriver settings
 PATH = "msedgedriver.exe"
@@ -31,6 +33,31 @@ def SetConsoleColor(Color):
     elif Color.lower() == 'reset':
         sys.stdout.write("\033[0;0m")
 
+def likeDislike(dislike, current_time, hitWord):
+    action = ActionChains(driver)
+    if dislike:
+        action.send_keys(Keys.ARROW_LEFT)
+        action.perform()
+        return
+    if autolike:
+        SetConsoleColor('Green')
+        print('[{}] Autoliked a profile with keyword "{}"\n'.format(current_time, hitWord))
+        SetConsoleColor('Reset')
+        action.send_keys(Keys.ARROW_RIGHT)
+        action.perform()
+    else:
+        SetConsoleColor('Green')
+        print('[{}] Found profile with keyword "{}"\n           Like? (Y/N)'.format(current_time, hitWord))
+        x = input('           ')
+        print()
+        SetConsoleColor('Reset')
+        if x.lower() == 'y':
+            action.send_keys(Keys.ARROW_RIGHT)
+            action.perform()
+        else:
+            action.send_keys(Keys.ARROW_LEFT)
+            action.perform()
+        
 def Swipe():
 
     bioElem = '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div[3]'  
@@ -38,9 +65,6 @@ def Swipe():
 
     try:
         while(True):
-
-            action = ActionChains(driver)
-
             #kill popups if there are any
             try:
                 driver.find_element_by_xpath(popup1).click()
@@ -75,35 +99,54 @@ def Swipe():
 
             #if keyword exists and no red flag was found wait for input
             if hit and not redFlagHit:
-                SetConsoleColor('Green')
-                print('[{}] Found profile with keyword "{}".\n           Like? (Y/N)'.format(current_time, hitWord))
-                SetConsoleColor('Reset')
-                x = input('')
-                if x.lower() == 'y':
-                    action.send_keys(Keys.ARROW_RIGHT)
-                    action.perform()
-                else:
-                    action.send_keys(Keys.ARROW_LEFT)
-                    action.perform()
+                likeDislike(False, current_time, hitWord)
             else:
                 SetConsoleColor('Red')
                 if redFlagHit:
-                    print('[{}] Criteria not matched -> Red flag: {}\n'.format(current_time, redFlagWord))
+                    print('[{}] Criteria not matched -> Redflag: {}\n'.format(current_time, redFlagWord))
                 else:
                     print('[{}] Criteria not matched\n'.format(current_time))
                 SetConsoleColor('Reset')
-                action.send_keys(Keys.ARROW_LEFT)
-                action.perform()
+                likeDislike(True, current_time, hitWord)
     except:
-        SetConsoleColor('Green')
-        print('[{}] No profiles left to swipe (or other exception).\nRetry? (Y/N)'.format(time.strftime("%H:%M:%S", time.localtime())))
-        SetConsoleColor('Reset')
-        x = input('')
+        while True:
+            cls()
+            SetConsoleColor('Green')
+            print('[{}] No profiles left to swipe (or other exception).\n[ R = Retry | C = Config | X = Exit ]'.format(time.strftime("%H:%M:%S", time.localtime())))
+            x = input('')
+            SetConsoleColor('Reset')
+            if x.lower() == 'r':
+                cls()
+                Swipe()
+            if x.lower() == 'c':
+                printConfig()
+            if x.lower() == 'x':
+                SetConsoleColor('Red')
+                print('Shutting down...')
+                SetConsoleColor('Reset')
+                try:
+                    driver.close()
+                except:
+                    None
+                exit()
 
-        if x.lower() == 'y':
-            Swipe()
-        else:
-            driver.close()
+#print config
+def printConfig():
+    while True:
+        cls()
+        global autolike
+        SetConsoleColor('Green')
+        print('Current Tin Bot configuration:\n')
+        print('Keywords: {}\n'.format(keywords))
+        print('Redflags: {}\n'.format(redFlags))
+        print('Autolike: {}\n'.format(autolike))
+        print('[ T = Toggle Autolike | B = Back ]')
+        x = input('')
+        SetConsoleColor('Reset')
+        if x.lower() == 't':
+            autolike = not autolike
+        if x.lower() == 'b':
+            return
 
 #initial start actions
 def init():
@@ -112,26 +155,39 @@ def init():
     time.sleep(0.5)
     driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[1]/button').click() #cookies
     driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a').click() #log in
+    time.sleep(0.5)
+    driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[3]/button').click() #log in method
     cls()
 
     SetConsoleColor('Green')
     print('Please sign in and wait for the next page to load.\nContinue? (Y/N)\n')
-    SetConsoleColor('Reset')
     x = input('')
+    SetConsoleColor('Reset')
     if x.lower() == 'y':
         cls()
         Swipe()
     else:
         driver.close()
 
-#startup
-cls()
-SetConsoleColor('Green')
-print('Welcome to Tin Bot ©2021, Rene Huber')
-print('Current keywords: {}\nStart? (Y/N)\n'.format(keywords))
-SetConsoleColor('Reset')
-x = input('')
-if x.lower() == 'y':
-    init()
-else:
-    driver.close()
+#program loop
+while True:
+    cls()
+    SetConsoleColor('Green')
+    print('Welcome to Tin Bot ©2021, Rene Huber')
+    print('[ S = Start | C = Config | X = Exit ]')
+    x = input('')
+    SetConsoleColor('Reset')
+    if x.lower() == 's':
+        init()
+    if x.lower() == 'c':
+        printConfig()
+    if x.lower() == 'x':
+        SetConsoleColor('Red')
+        print('Shutting down...')
+        SetConsoleColor('Reset')
+        try:
+            driver.close()
+        except:
+            None
+        exit()
+    
